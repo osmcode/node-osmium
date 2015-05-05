38e227d005cd8d78043f872c3b427e4f1d511e2d
Dane Springmeyer <dane@mapbox.com>
2015-05-04 18:26:52 -0700

    don't publish on coverage run

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
 
-./node_modules/.bin/node-pre-gyp package testpackage
+COVERAGE=${COVERAGE:-false}
 
-COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
+if [[ ${COVERAGE} == false ]]; then
+    ./node_modules/.bin/node-pre-gyp package testpackage
 
-if [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]; then
-    ./node_modules/.bin/node-pre-gyp publish
-elif [[ ${COMMIT_MESSAGE} =~ "[republish binary]" ]]; then
-    ./node_modules/.bin/node-pre-gyp unpublish publish
-fi;
+    COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
+
+    if [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]; then
+        ./node_modules/.bin/node-pre-gyp publish
+    elif [[ ${COMMIT_MESSAGE} =~ "[republish binary]" ]]; then
+        ./node_modules/.bin/node-pre-gyp unpublish publish
+    fi
+fi
