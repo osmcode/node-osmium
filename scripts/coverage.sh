e5b67703a579b72d88732aa69854d81c0cb7d358
Dane Springmeyer <dane@mapbox.com>
2016-03-30 08:28:01 -0700

    build updates: coverage on linux, improved node install, bump to libosmium 2.6.1

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,6 +4,8 @@ COVERAGE=${COVERAGE:-false}
 
 if [[ ${COVERAGE} == true ]]; then
     ./mason_packages/.link/bin/cpp-coveralls \
+      --gcov /usr/bin/llvm-cov-3.5 \
+      --exclude __nvm \
       --exclude src/node_osmium.hpp \
       --exclude src/apply.hpp \
       --exclude src/include_v8.hpp \
